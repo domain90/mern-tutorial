5a49d3c450aedb349295bec51dd26afc4be75f4c
Giwoo G Lee <gglee89@gmail.com>
2016-12-11 16:35:05 +0900

    5000

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(express.static(__dirname));
 //app.use("/bower_components", express.static(__dirname + "/bower_components"));
 app.use("/",router);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
