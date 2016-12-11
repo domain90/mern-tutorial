228d2cc9c62bf0977962a0272a6639694705cb05
Giwoo G Lee <gglee89@gmail.com>
2016-12-11 15:22:15 +0900

    CSS is working!!

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-app.get("/", function(resquest, response){
-    response.send("Hello World");
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.listen(3000);
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/bower_components", express.static(__dirname + "/bower_components"));
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
